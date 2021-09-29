754f380c99756eba921f9c4ab990080ef8d2ba19
yogesh <etpr.yogesh@gmail.com>
2021-09-29 14:45:14 +0530

    routes have been updated now app is working

--- routes/list.js
+++ routes/list.js
@@ -9,6 +9,7 @@ const verify = require("../middlewares/verify");
 
 //router.get("/", auth, catchErrors(listController.getAllList));
 router.post("/create", auth, catchErrors(listController.createList));
+router.get("/create", auth, catchErrors(listController.createLists));
 router.get("/:id", auth, catchErrors(listController.getList)); //get list by list id or get all list of user
 router.put("/:id", auth, catchErrors(listController.updateList));
 router.delete("/:id", auth, catchErrors(listController.deleteList))
@@ -18,6 +19,6 @@ router.delete("/:id", auth, catchErrors(listController.deleteList))
 //this route is to get lists according to filters as well as without filters
 //in query pass category/title to find or sort= new/old to find list
 router.get("/user/", auth, catchErrors(listController.getUserList));
-router.get("/", auth, catchErrors(listController.getAllListAdmin));
+router.get("/all", auth, catchErrors(listController.getAllListAdmin));
 
 module.exports = router;
